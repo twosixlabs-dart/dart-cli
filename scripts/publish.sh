a7f1c20cc40146d05cfe095c2d16645d14b1ce00
johnhungerford <hungerfordjustice@gmail.com>
2022-04-01 13:17:51 -0400

    Logging git hash -- why isnt it working?

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -6,8 +6,10 @@ if [[ -z "$PYPI_USERNAME" ]]; then
     echo "Publishing test package: expect to be prompted for username and password"
     python -m twine upload --repository testpypi dist/*
 else
+    echo "$GITHUB_SHA"
     git_hash=$(git rev-parse --short "$GITHUB_SHA")
-    python scripts/manage_version.py update-version "-${git_hash}"
+    echo "$git_hash"
+    python scripts/manage_version.py update-version "-$git_hash"
     python3 setup.py build sdist bdist_wheel
     if grep -q latest app.version; then
         python -m twine upload --repository testpypi --verbose dist/*  -u "$PYPI_USERNAME" -p "$PYPI_PASSWORD"
