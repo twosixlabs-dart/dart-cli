1e84b761173dfa85b3e7a8d582f142f544dabdea
johnhungerford <hungerfordjustice@gmail.com>
2022-04-01 11:35:48 -0400

    Updated version/publication scripts; added github workflow for test and publish

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
 python3 setup.py build sdist bdist_wheel
+
+if [[ -z "$PYPI_USERNAME" ]]; then
+    echo "Publishing test package: expect to be prompted for username and password"
+    python -m twine upload --repository testpypi dist/*
+else
+    if grep -q latest app.version; then
+        python -m twine upload --repository testpypi --verbose dist/*  -u "$PYPI_USERNAME" -p "$PYPI_PASSWORD"
+    else
+        python -m twine upload dist/* --verbose -u "$PYPI_USERNAME" -p "$PYPI_PASSWORD"
+    fi
+fi
+
