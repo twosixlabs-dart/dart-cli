14cd2748be56835b82408d41287754d65235fa59
johnhungerford <hungerfordjustice@gmail.com>
2022-04-01 13:23:05 -0400

    Passing -github_sha as argument, not option

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -9,7 +9,7 @@ else
     echo "$GITHUB_SHA"
     git_hash=$(git rev-parse --short "$GITHUB_SHA")
     echo "$git_hash"
-    python scripts/manage_version.py update-version "-$git_hash"
+    python scripts/manage_version.py update-version -- "-$git_hash"
     python3 setup.py build sdist bdist_wheel
     if grep -q latest app.version; then
         python -m twine upload --repository testpypi --verbose dist/*  -u "$PYPI_USERNAME" -p "$PYPI_PASSWORD"
